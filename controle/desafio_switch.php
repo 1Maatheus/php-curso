9882ccbde8da86cf274bad9d08ec9649c3654dc9
Matheus Oliveira Monteiro <matheusoliveira7227@gmail.com>
2024-03-04 13:01:00 -0300

    Entrega de desafio switch

--- controle/desafio_switch.php
+++ controle/desafio_switch.php
@@ -7,6 +7,8 @@
     <option value="milha-km">Milha > Km</option>
     <option value="metro-km">Metro > Km</option>
     <option value="km-metro">Km > Metro</option>
+    <option value="c-f">Celsius > Fahrenheit</option>
+    <option value="f-c">Fahrenheit > Celsius</option>
   </select>
 
   <button>Calcular</button>
@@ -38,6 +40,14 @@ switch($_POST['conversao']) {
   case 'km-metro':
     $param = $_POST['param'] * 1000;
     echo $mensagem = "{$_POST['param']} Km(s) = $param Metro(s)";
+    break;
+  case 'c-f':
+    $param = ($_POST['param'] * 9/5) + 32;
+    echo $mensagem = "{$_POST['param']} °C = $param °F";
+    break;
+  case 'f-c':
+    $param = ($_POST['param'] - 32) * 5/9;
+    echo $mensagem = "{$_POST['param']} °F = $param °C";
     break;
   default:
   echo $mensagem = "Nenhum valor calculado";
