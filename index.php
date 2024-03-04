afbb4478c6aaa6d72adf819434567ec977a58bfe
Matheus Oliveira Monteiro <matheusoliveira7227@gmail.com>
2024-03-04 12:10:00 -0300

    Adição de novo desafio

--- index.php
+++ index.php
@@ -30,6 +30,9 @@
                 <li>
                   <a href="exercicio.php?dir=controle&file=operadores_logicos" >Operadores Lógicos</a>
                 </li>
+                <li>
+                  <a href="exercicio.php?dir=controle&file=desafio_operadores_logicos" >Desafio Operadores Lógicos</a>
+                </li>
               </ul>
           </div>
 
