8dc8c86e8e64aac6c7aeca0a71f5ff2d3e29bbf4
Matheus Oliveira Monteiro <matheusoliveira7227@gmail.com>
2024-03-08 13:44:52 -0300

    Adição de parte teórica

--- classes_objetos/classe.php
+++ classes_objetos/classe.php
@@ -1,1 +1,24 @@
 <div class="titulo">Classe</div>
+
+<?php
+class Cliente {
+    public $nome = 'Anônimo';
+    public $idade = 18;
+
+    public function apresentar() {
+        echo "Nome: {$this->nome} <br> Idade: {$this->idade} <br>";
+    }
+}
+
+$c1 = new Cliente();
+$c1 -> nome = "Matheus Oliveira Monteiro";
+$c1 -> idade = 25;
+$c1 -> apresentar();
+
+echo "<br>";
+echo "<hr>";
+
+$c2 = new Cliente();
+$c2 -> nome = "Nayara Sanches Dall'ara";
+$c2 -> idade = 25;
+$c2 -> apresentar();
