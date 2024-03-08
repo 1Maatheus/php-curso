f53d44b0542bbb9924d6e9ec4f162dbf8b4ab9b7
Matheus Oliveira Monteiro <matheusoliveira7227@gmail.com>
2024-03-08 14:06:20 -0300

    Adição de novo exemplo de class

--- classes_objetos/construtor_destrutor.php
+++ classes_objetos/construtor_destrutor.php
@@ -1,3 +1,29 @@
 <div class="titulo">Construtor e Destrutor</div>
 
 <?php
+
+class Pessoa {
+  public $nome;
+  public $idade;
+
+  function __construct($novoNome, $idade = 18) {
+    echo 'Construtor invocado! <br>';
+    $this->nome = $novoNome;
+    $this->idade = $idade;
+  }
+
+  function __destruct() {
+    echo 'E morreu! <br>';
+  }
+
+  public function apresentar() {
+    return "{$this->nome}, {$this->idade} anos<br>";
+  }
+}
+ 
+$pessoa = new Pessoa('Matheus', 25);
+$pessoa2 = new Pessoa('Nayara', 25);
+echo $pessoa->apresentar();
+echo $pessoa2->apresentar();
+
+unset($pessoa);
