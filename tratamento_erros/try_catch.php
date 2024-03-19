2b9319a1249e8c358a2618e9859f8a931a6d21e8
Matheus Oliveira Monteiro <matheusoliveira7227@gmail.com>
2024-03-19 11:03:10 -0300

    Adição de primeiros exemplos

--- tratamento_erros/try_catch.php
+++ tratamento_erros/try_catch.php
@@ -1,1 +1,21 @@
 <div class="titulo">Try/Catch</div>
+
+<?php
+try {
+    echo intdiv(7, 0) . '<br>';
+} catch (Error $e) {
+    echo 'Erro encontrado: ' . $e->getMessage() . '<br>';
+}
+
+try {
+  throw new Exception('Um erro muito estranho aconteceu');
+  echo intdiv(7, 0) . '<br>';
+} catch(DivisionByZeroError $e) {
+  echo 'Divisão por zero<br>';
+} catch(Throwable $e) {
+  echo 'Erro encontrado: ' . $e->getMessage() . '<br>';
+} finally {
+  echo 'Sempre executado<br>';
+}
+
+echo 'Execução continua...<br>';
